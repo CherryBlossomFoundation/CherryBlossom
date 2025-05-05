e7bbbe99b7fb7b67fcc45089b049c8ce4bcebb43
Jjoon0513 <ijjun244@gmail.com>
2025-05-05 21:58:20 +0900

    훔

--- cherryu/parse/parsecodedef.py
+++ cherryu/parse/parsecodedef.py
@@ -4,9 +4,9 @@ from .type import rettypes, vartypes
 
 
 def parse_def(c_lines: list[str], line: str, lineno: int) -> bool:
-    defic = re.match(r'^([a-zA-Z_]\w*)[\t ]*\((.*?)\)[\t ]*$', line)
+    defic = re.match(r'^([a-zA-Z_.]\w*)[\t ]*\((.*?)\)[\t ]*$', line)
 
-    retfic = re.match(r'^return[\t ]+([a-zA-Z_]\w*)[\t ]*\((.*?)\)[\t ]*$', line)
+    retfic = re.match(r'^return[\t ]+([a-zA-Z_.]\w*)[\t ]*\((.*?)\)[\t ]*$', line)
 
     if retfic:
         c_lines.append(f'return {retfic.group(1)}({retfic.group(2)});')
